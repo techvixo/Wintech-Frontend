4e35328b94df16ad2b18d80ee2b4614a7150294a
sultanmahmud07 <asadkhan01862@gmail.com>
2024-12-14 23:21:24 +0600

    Order processing transilate added

--- src/components/Pages/Servces/ServiceDetails/GetStarted/GetStarted.jsx
+++ src/components/Pages/Servces/ServiceDetails/GetStarted/GetStarted.jsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import img1 from "../../../../../../public/assets/services/flags/1.png";
 import img2 from "../../../../../../public/assets/services/flags/2.png";
 import img3 from "../../../../../../public/assets/services/flags/3.png";
@@ -7,7 +8,9 @@ import img6 from "../../../../../../public/assets/services/flags/6.png";
 import img7 from "../../../../../../public/assets/services/flags/7.png";
 import Image from "next/image";
 import Link from "next/link";
+
 const GetStarted = () => {
+  const t = useTranslations('ServicePage');
   const flags = [
     {
       id: 1,
@@ -50,14 +53,14 @@ const GetStarted = () => {
     <div className=" py-3 md:py-8">
       <div className="main_container">
         <h2 className="text-center text-2xl md:text-4xl font-bold text-[#070F11] my-4 md:my-7">
-          Let’s Get Started
+        {t(`GetStart.title`)}
         </h2>
         <div className="flex items-center justify-center py-3 md:my-5">
         <Link
           href={`/services`}
          className="shadow md:shadow-md font-semibold bg-gradient-to-r from-[#4177B7] to-[#86BCFC]  hover:bg-gradient-to-r hover:from-[#86BCFC] hover:to-[#4177B7] p-2 text-base md:text-lg px-3 md:px-10 rounded-md text-[#FFFFFF]  transition"
         >
-        Get Instant Quote
+         {t(`GetStart.button`)}
         </Link>
         </div>
         <div className="flex items-center justify-center flex-wrap gap-2 pt-3">
--- src/components/Pages/Servces/ServiceDetails/OrderingProcess/OrderingProcess.jsx
+++ src/components/Pages/Servces/ServiceDetails/OrderingProcess/OrderingProcess.jsx
@@ -1,4 +1,8 @@
+import { useTranslations } from "next-intl";
+
 const OrderingProcess = () => {
+  const t = useTranslations('ServicePage');
+  const keys = ['step1', 'step2', 'step3', 'step4', 'step5'];
   const steps = [
     {
       title: "Get a Quote",
@@ -28,7 +32,7 @@ const OrderingProcess = () => {
     <div className=" py-3 md:pb-8">
       <div className="main_container p-3 md:p-6 bg-white shadow md:shadow-lg rounded-md">
         <h2 className="text-center text-2xl md:text-3xl font-bold text-[#070F11] my-4 md:mb-7">
-          Our Ordering Process
+        {t(`ProcessSteps.title`)}
         </h2>
          {/* Horizontal line */}
          <div className="flex justify-center top-9 left-0  right-0 w-full py-3 md:py-4">
@@ -43,17 +47,17 @@ const OrderingProcess = () => {
             </div>
           </div>
         <div className="grid grid-cols-2 md:grid-cols-5 gap-3 pt-3  w-full">
-         {steps.map((step, index) => (
+         {keys.map((step, index) => (
             <div
               key={index}
               className="md:pt-5 flex flex-col items-center text-center"
             >
               {/* <div className="w-3 h-3 bg-gray-800 rounded-full mb-10 hidden md:block" /> */}
               <h3 className="font-semibold text-gray-800 text-md">
-                {step.title}
+              {t(`ProcessSteps.StepsList.${step}.title`)}
               </h3>
               <p className="text-gray-600 text-sm md:text-base mt-1">
-                {step.description}
+              {t(`ProcessSteps.StepsList.${step}.description`)}
               </p>
             </div>
           ))}
